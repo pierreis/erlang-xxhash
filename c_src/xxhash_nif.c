1fbd62b538cab9abd9d103ccb3c41821c38802b2
Pierre Matri <pmatri@acute.fr>
2013-05-20 18:00:21 +0200

    Initial commit.

---
+++ c_src/xxhash_nif.c
@@ -0,0 +1,140 @@
+#include "erl_nif.h"
+#include "xxhash.h"
+
+#define UNUSED(x) (void)(x)
+
+
+// Misc. definitions
+
+void xxhash_cleanup(ErlNifEnv *env, void *p);
+
+typedef struct handle {
+  void *instance;
+} handle_t;
+
+
+// Erlang terms and exports.
+
+static ERL_NIF_TERM nif_hash32(ErlNifEnv *env, int argc,
+  const ERL_NIF_TERM argv[]);
+static ERL_NIF_TERM nif_hash32_init(ErlNifEnv *env, int argc,
+  const ERL_NIF_TERM argv[]);
+static ERL_NIF_TERM nif_hash32_update(ErlNifEnv *env, int argc,
+  const ERL_NIF_TERM argv[]);
+static ERL_NIF_TERM nif_hash32_final(ErlNifEnv *env, int argc,
+  const ERL_NIF_TERM argv[]);
+
+static ERL_NIF_TERM atom_ok;
+
+static ErlNifFunc nif_funcs[] = {
+  {"hash32_impl", 2, nif_hash32},
+  {"hash32_init_impl", 1, nif_hash32_init},
+  {"hash32_update_impl", 2, nif_hash32_update},
+  {"hash32_final_impl", 1, nif_hash32_final}
+};
+
+static ErlNifResourceType *xxhash_handle;
+
+
+// Hash32.
+
+static ERL_NIF_TERM
+nif_hash32(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+  ErlNifBinary src_bin;
+  unsigned int hash;
+  unsigned int seed;
+  UNUSED(argc);
+
+  if (!enif_inspect_binary(env, argv[0], &src_bin) ||
+      !enif_get_uint(env, argv[1], &seed)) {
+    return enif_make_badarg(env);
+  }
+
+  hash = XXH32(src_bin.data, src_bin.size, seed);
+
+  return enif_make_uint(env, hash);
+}
+
+
+static ERL_NIF_TERM
+nif_hash32_init(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+  handle_t *handle = enif_alloc_resource(xxhash_handle, sizeof(handle_t));
+  unsigned int seed;
+  ERL_NIF_TERM term;
+  UNUSED(argc);
+
+  if (!enif_get_uint(env, argv[0], &seed)) {
+    return enif_make_badarg(env);
+  }
+
+  handle->instance = XXH32_init(seed);
+
+  term = enif_make_resource(env, handle);
+
+  return term;
+}
+
+static ERL_NIF_TERM
+nif_hash32_update(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+  handle_t *handle;
+  ErlNifBinary src_bin;
+  UNUSED(argc);
+
+  if (!enif_get_resource(env, argv[0], xxhash_handle, (void **) &handle) ||
+      !enif_inspect_binary(env, argv[1], &src_bin)) {
+    return enif_make_badarg(env);
+  }
+
+  XXH32_update(handle->instance, src_bin.data, src_bin.size);
+
+  return atom_ok;
+}
+
+static ERL_NIF_TERM
+nif_hash32_final(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+  handle_t* handle;
+  unsigned int hash;
+  UNUSED(argc);
+
+  if (!enif_get_resource(env, argv[0], xxhash_handle, (void **) &handle)) {
+    return enif_make_badarg(env);
+  }
+
+  hash = XXH32_digest(handle->instance);
+
+  return enif_make_uint(env, hash);
+}
+
+
+
+
+// Loading and cleaning stuff.
+
+static int init(ErlNifEnv *env, void* *priv_data, ERL_NIF_TERM load_info) {
+  UNUSED(priv_data);
+  UNUSED(load_info);
+  atom_ok = enif_make_atom(env, "ok");
+
+  if ((xxhash_handle = enif_open_resource_type(env, "xxhash_nif",
+        "xxhash_handle", &xxhash_cleanup,
+        ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL)) == NULL) {
+    return -1;
+  }
+
+  return 0;
+}
+
+void xxhash_cleanup(ErlNifEnv *env, void *obj) {
+  handle_t *handle;
+  UNUSED(env);
+
+  handle = (handle_t *) obj;
+
+  XXH32_digest(handle->instance);
+  enif_release_resource(handle);
+}
+
+
+// Init stuff.
+
+ERL_NIF_INIT(xxhash, nif_funcs, &init, NULL, NULL, NULL);
---
+++ src/xxhash.erl
@@ -0,0 +1,133 @@
+%%% @author Pierre Matri  <pierre@matri.me>
+%%%
+%%% @copyright 2013 Pierre Matri, All rights reserved. Open source, BSD License.
+%%% @version 0.1.0
+
+-module(xxhash).
+-version(0.1).
+
+-define(DEFAULT_SEED, 0).
+
+-export([hash32/1, hash32/2,
+         hash32_init/0, hash32_init/1,
+         hash32_update/2, hash32_final/1]).
+
+-on_load(init/0).
+
+-spec init() -> ok | {error, any()}.
+
+-define(nif_stub, nif_stub_error(?LINE)).
+nif_stub_error(Line) ->
+    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, Line}).
+
+
+init() ->
+    PrivDir = case code:priv_dir(?MODULE) of
+                  {error, bad_name} ->
+                      EbinDir = filename:dirname(code:which(?MODULE)),
+                      AppPath = filename:dirname(EbinDir),
+                      filename:join(AppPath, "priv");
+                  Path ->
+                      Path
+              end,
+
+    erlang:load_nif(filename:join(PrivDir, xxhash_nif), 0).
+
+
+%%%=============================================================================
+%%% Nif functions
+%%%=============================================================================
+
+hash32_impl(_Data, _Seed) ->
+  ?nif_stub.
+
+hash32_init_impl(_Seed) ->
+  ?nif_stub.
+
+hash32_update_impl(_Context, _Data) ->
+  ?nif_stub.
+
+hash32_final_impl(_Context) ->
+  ?nif_stub.
+
+
+%%%=============================================================================
+%%% Exports
+%%%=============================================================================
+
+hash32(Data) ->
+  hash32(Data, ?DEFAULT_SEED).
+
+hash32(Data, Seed) when is_binary(Data) andalso is_integer(Seed) ->
+  hash32_impl(Data, Seed);
+hash32(Data, Seed) when is_list(Data) andalso is_integer(Seed) ->
+  hash32_impl(list_to_binary(Data), Seed);
+hash32(Data, Seed) when is_atom(Data) andalso is_integer(Seed) ->
+  hash32_impl(atom_to_binary(Data, utf8), Seed);
+hash32(Data, Seed) when is_integer(Data) andalso is_integer(Seed) ->
+  hash32_impl(list_to_binary(integer_to_list(Data)), Seed);
+hash32(Data, Seed) when is_float(Data) andalso is_integer(Seed) ->
+  hash32_impl(<<Data/float>>, Seed).
+
+hash32_init() ->
+  hash32_init(?DEFAULT_SEED).
+
+hash32_init(Seed) when is_integer(Seed) ->
+  hash32_init_impl(Seed).
+
+hash32_update(Context, Data) when is_binary(Data) andalso is_binary(Context) ->
+  hash32_update_impl(Context, Data).
+
+hash32_final(Context) when is_binary(Context) ->
+  hash32_final_impl(Context).
+
+%%%=============================================================================
+%%% Tests
+%%%=============================================================================
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+hash32_test() ->
+  Hash1 = hash32(<<"FooBar">>),
+  Handle = hash32_init(),
+  ok = hash32_update(Handle, <<"Foo">>),
+  ok = hash32_update(Handle, <<"Bar">>),
+  Hash2 = hash32_final(Handle),
+  ?assertEqual(Hash1, Hash2).
+
+hash32_seed_test() ->
+  Hash1 = hash32(<<"FooBar">>, 50),
+  Handle = hash32_init(50),
+  ok = hash32_update(Handle, <<"Foo">>),
+  ok = hash32_update(Handle, <<"Bar">>),
+  Hash2 = hash32_final(Handle),
+  ?assertEqual(Hash1, Hash2).
+
+hash32_seed2_test() ->
+  Hash1 = hash32(<<"FooBar">>, 50),
+  Hash2 = hash32(<<"FooBar">>),
+  ?assertNot(Hash1 == Hash2).
+
+hash32_list_test() ->
+  Hash1 = hash32([1, 42]),
+  Hash2 = hash32([1, 220]),
+  ?assertNot(Hash1 == Hash2).
+
+hash32_atom_test() ->
+  Hash1 = hash32(foo),
+  Hash2 = hash32(bar),
+  ?assertNot(Hash1 == Hash2).
+
+hash32_int_test() ->
+  Hash1 = hash32(42),
+  Hash2 = hash32(67),
+  ?assertNot(Hash1 == Hash2).
+
+hash32_float_test() ->
+  Hash1 = hash32(5.98),
+  Hash2 = hash32(5.97),
+  ?assertNot(Hash1 == Hash2).
+
+-endif.
