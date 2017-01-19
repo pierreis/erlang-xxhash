25666ff5db6078e0c106cda6ddee1047463a7f4c
Sungjin Park <jinni.park@gmail.com>
2017-01-19 15:43:25 +0900

    Fix memory leak
    An enif_release_resource must correspond to each enif_alloc_resource.

--- c_src/xxhash_nif.c
+++ c_src/xxhash_nif.c
@@ -77,6 +77,7 @@ nif_hash32_init(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
   }
   XXH32_reset(state, seed);
   term = enif_make_resource(env, state);
+  enif_release_resource(state);
   return term;
 }
 
@@ -137,6 +138,7 @@ nif_hash64_init(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
   }
   XXH64_reset(state, seed);
   term = enif_make_resource(env, state);
+  enif_release_resource(state);
   return term;
 }
 
